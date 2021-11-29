5eab37e6a56e9802dad72fdd0be58493cbb035c1
wdiogu <wdiogu@gmail.com>
2021-11-29 09:27:12 -0500

    added new task.json

---
+++ tmg/aimsun_playground/src/Aimsun/assignment/macroscopic_assignment.py
@@ -0,0 +1,35 @@
+import sys
+import time
+from PyANGBasic import *
+from PyANGKernel import *
+from PyANGConsole import *
+import shlex
+
+# Main script to complete the full netowrk import
+def main(argv):
+    overallStartTime = time.perf_counter()
+    if len(argv) < 3:
+        print("Incorrect Number of Arguments")
+        print("Arguments: -script script.py campnou_network.ang")
+        return -1
+    # Start a console
+    console = ANGConsole()
+    # Load a network
+    if console.open(argv[1]):
+        global model
+        model = console.getModel()
+        print("open campnou network")
+    else:
+        console.getLog().addError("Cannot load the network")
+        print("cannot load network")
+        return -1
+
+    overallEndTime = time.perf_counter()
+    print(f"Overall Runtime: {overallEndTime-overallStartTime}s")
+    # Reset the Aimsun undo buffer
+    model.getCommander().addCommand(None)
+    return 0
+
+
+if __name__ == "__main__":
+    main(sys.argv)
