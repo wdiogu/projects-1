c9ef44318f6688ed6824f0441ca103196885c26e
amitsandhel <amit.sandhel@gmail.com>
2021-12-01 11:23:08 -0500

    Successfully able to add new columns aka new attributes to UserClass Object.

--- aimsun_playground/src/Aimsun/assignment/macroscopic_assignment.py
+++ aimsun_playground/src/Aimsun/assignment/macroscopic_assignment.py
@@ -6,8 +6,11 @@ from PyANGConsole import *
 import shlex
 
 
-def vdf(context, section, funcVolume):
 
+
+def vdf(context, section, funcVolume):
+    """this function is the default function found in the ang file we were playing with
+    over the weekend keepting it here for backup and reference"""
     volume = funcVolume.getVolume()
     capacity = section.getCapacity()
     addVolume = section.getAdditionalVolume()
@@ -36,8 +39,10 @@ def vdf(context, section, funcVolume):
     return time
 
 def loadModel(filepath, console):
+    """to load the model"""
     if console.open(filepath):
-        model = console.getModel()
+        #model = console.getModel()
+        model = GKSystem.getSystem().getActiveModel()
         print("Open network")
     else:
         console.getLog().addError("Cannot load the network")
@@ -47,8 +52,124 @@ def loadModel(filepath, console):
     geomodel = model.getGeoModel()
     return model, catalog, geomodel
 
-def extract_feature():
-    pass
+def addColumn(userClassType, arg_list):
+    #userClassType.addColumn( "GKUserClass::value_of_time_python", "value_of_time_python", GKColumn.Double, GKColumn.eExternal ) 
+    userClassType.addColumn( arg_list[0], arg_list[1], arg_list[2] ) 
+    print ('external was built')
+    #return userClassType
+
+def save(console, model):
+    console.save("C:\\Users\\sandhela\\source\\repos\\williamsProject1\\aimsun_playground\\input_files\\aimsun_files\\test2.ang")
+    # Reset the Aimsun undo buffer
+    model.getCommander().addCommand( None )
+    print ("Network saved Successfully")
+
+def userClass(model, console,  catalog, geomodel):
+    sectionType = model.getType("GKSection")
+    userClassType = model.getType("GKUserClass")
+    print (sectionType)
+    print (dir(sectionType))
+    print (dir(userClassType))
+    print (userClassType.getName())
+    cols = userClassType.getColumns( GKType.eSearchOnlyThisType )
+    #print (cols)
+    for item in cols:
+        print (item, item.getName(), item.getId(), item.getExternalName(), str(item.getColumnType()))
+    #print (dir(item))
+
+    #add a new column
+    addColumn(userClassType, ["GKUserClass::value_of_time_python", "value_of_time_python", GKColumn.Double, GKColumn.eExternal])
+    addColumn(userClassType, ["GKUserClass::value_of_time2_python", "value_of_time2_python", GKColumn.Double, GKColumn.eExternal])
+    
+    #save the model 
+    save(console, model)
+    
+
+    # attributes  = uc.getColumns( GKType.eSearchOnlyThisType )
+    # for item in attributes:
+    #     print (item, item.getName())
+
+    #system = GKSystem.getSystem()
+    #g_object = model.getType("GKObject")
+    #print (g_object, g_object.getName())
+    #print (dir(g_object))
+    #ans = model.getCatalog().getObjectsByType(model.getType("GKUserClass"))
+    #print (ans)
+    #print (dir(ans[1043]))
+    #attributes  = sectionType.getColumns( GKType.eSearchOnlyThisType )
+
+
+
+    #user_class = model.getType("GKUserClass")
+    #print (dir(catalog))
+    
+    #valueoftime = user_class.value_of_time()
+    #print (valueoftime)
+
+    # user_class_object = catalog.getObjectsByTypeWithSubTypes(user_class) #getObjectsByType(user_class)
+    # print (user_class_object)
+    # for item in user_class_object:
+    #      print (item, item.getName() ) #user_class_object[item].getName())
+    # print(dir(item))
+
+    #print (dir(user_class_object[item]))
+    #vehicle = model.userClass.getVehicle()
+    # context = model.getType("GKFunctionCostContext")
+    # print (str(context))
+    # dta = model.getType("DTAManager")
+    # print (dta)
+    # print (dir(dta))
+    
+    #GKUserClass
+
+    #print (dir(system))
+    
+    #AKIGetSectionUserDefinedCost()
+    #AKIVehGetNbVehTypes
+    
+    #context is a GKFunctionCostContext python object. 
+    #manager is a DTAManager python object. 
+    #section is a DTASection python object. 
+
+  
+    #userClass = model.getType( "GKUserClass")
+    #print (userClass)
+    #print (dir(catalog))
+    #print (dir(userClass))
+    #this is a list of all userClass objects but this doesn't access the attributes as 
+    #cleanly as we would like
+    #userClassCatalog = catalog.getUsedSubTypesFromType(userClass)
+    #userClassDict = userClassCatalog[0]
+    #print ('userclassdict ', userClassDict)
+    #for item in userClassDict:
+    #    print (item, userClassDict[item].getName())
+    #print (dir(userClassDict[item]))
+
+    #to access the attributes of this class this outputs a huge list of pyangkernel objects
+    # attributes  = userClass.getColumns( GKType.eSearchOnlyThisType )
+    # #print ('attribures ', attributes)
+    # #we need to iterate over it no choice to find the attribute of interest 
+    # for item in attributes:
+    #     print (item, item.getName())
+    #     if item.getExternalName() == "value_of_time":
+    #         #print ('yes')
+    #         # print (item)
+    #         # print (dir(item))
+    #         # print (item.getContents())
+    #         # print (item.getName())
+    #         #xxx = userClass.getDataValueDouble(item)
+    #         #print (xxx)
+    #         break
+
+            
+    #for x in attributes:
+    #    print (x, str(x.getName()), str(x.getExternalName()) )
+    #print (dir(x))
+    #     if x.getExternalName() == "value_of_time":
+    #         value_of_time = context.userClass.getDataValueDouble(x)
+    #         break
+
+    
 
 # Main script to complete the full netowrk import
 def main(argv):
@@ -64,6 +185,7 @@ def main(argv):
     print ('argv: ', argv)
     # generate a model of the input network
     model, catalog, geomodel = loadModel(Network, console)
+    userClass(model, console, catalog, geomodel)
 
     overallEndTime = time.perf_counter()
     print(f"Overall Runtime: {overallEndTime-overallStartTime}s")
