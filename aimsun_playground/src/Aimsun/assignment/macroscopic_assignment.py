c6c92e5ac90f30ee1b301ee297864c5a798260ee
wdiogu <wdiogu@gmail.com>
2021-12-03 15:16:40 -0500

    added ability to create gkobjects folders

--- aimsun_playground/src/Aimsun/assignment/macroscopic_assignment.py
+++ aimsun_playground/src/Aimsun/assignment/macroscopic_assignment.py
@@ -4,68 +4,65 @@ from PyANGBasic import *
 from PyANGKernel import *
 from PyANGConsole import *
 import shlex
+import import_matrix
 
 # Main script to complete the full netowrk import
 def main(argv):
     # overallStartTime = time.perf_counter()
     if len(argv) < 3:
         print("Incorrect Number of Arguments")
-        print("Arguments: -script script.py campnou_network.ang output_folder")
+        print(
+            "Arguments: -script script.py input_network.ang output_network.ang input_matrix.txt"
+        )
         return -1
     # Start a console
     console = ANGConsole()
     # Load a network
-    if console.open(argv[1]):
-        global model
-        model = console.getModel()
+    load_network(console, argv[1])
+
+    model = console.getModel()
+
+    centroid_config = create_gkobject(
+        "GKCentroidConfiguration", model, "Centroid Configuration"
+    )
+    add_folder_to_gkobject("GKModel::centroidsConf", model, centroid_config)
+
+    functions = create_gkobject("GKFunctionCost", model, "Amits_VDFs")
+    add_folder_to_gkobject("GKModel::functions", model, functions)
+
+    save_network(console, model, argv)
+
+
+def load_network(console, network_file):
+    if console.open(network_file):
         print("load campnou network")
     else:
         console.getLog().addError("Cannot load the network")
         print("cannot load network")
         return -1
-    centroids = matrix.getCentroidConfiguration().getCentroidsInOrder()
-    print(centroids)
 
+
+def create_gkobject(gk_object_internal_name, model, target_name):
+    gk_object = GKSystem.getSystem().newObject(str(gk_object_internal_name), model)
+    gk_object.setName(str(target_name + " " + str(gk_object.getId())))
+    return gk_object
+
+
+def add_folder_to_gkobject(internal_folder_name, model, gkobject):
+    folder_name = str(internal_folder_name)
+    folder = model.getCreateRootFolder().findFolder(folder_name)
+    if folder == None:
+        folder = GKSystem.getSystem().createFolder(
+            model.getCreateRootFolder(), folder_name
+        )
+    folder.append(gkobject)
+
+
+def save_network(console, model, argv):
+    console.save(argv[2])
     # Reset the Aimsun undo buffer
-    # model.getCommander().addCommand(None)
-
-
-def create_scenario():
-    """
-    - read traffic plan
-    read public transport plan
-    read path assignment plan
-    read master control plan
-    """
-    pass
-
-
-def create_experiment():
-    """
-    set assignment method
-    store path assingment to save_path_assignment_info()
-    """
-    ...
-
-
-def save_path_assignment_info():
-    """
-    set where to save file
-    """
-    ...
-
-
-def save_network():
-    ...
-
-
-def _execute(output_file, model, console):
-    """
-    create scenario
-    create experiment
-    save path assignment
-    run experiment
-    """
+    model.getCommander().addCommand(None)
+    print("Network saved Successfully")
 
 
 if __name__ == "__main__":
